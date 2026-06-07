48bd558bb8a542e8bbca89f18e635af6ec26d11d
Madhav Sharma <sharma89755@gmail.com>
2026-06-07 16:48:49 +0530

    Updated docker file and added dockercompose file to start both the containers for frontend and backend

--- backend/proj_movie_explorer_be/urls.py
+++ backend/proj_movie_explorer_be/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -14,7 +15,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def healthcheck(request):
+    return JsonResponse({'status': 'ok'})
+
 urlpatterns = [
+    path('', healthcheck, name='healthcheck'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
